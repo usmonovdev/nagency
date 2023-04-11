e271434f5bbf6991500c9d3ac342b06a073f12ae
Usmonov Azizbek <usmonovazizbek045@gmail.com>
2023-04-11 13:20:45 +0500

    Keyframes

--- src/components/home/testimonals/Tstimonals.vue
+++ src/components/home/testimonals/Tstimonals.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="testimonials-box">
-    <div class="testimonials" v-for="data in testimonal">
+    
+    <div class="testimonials first" v-for="data in testimonalFirst">
+      <img :src="data.photo" :alt="data.username" />
+      <p>{{ data.message }} - @{{ data.username }}</p>
+    </div>
+    <div class="testimonials second" v-for="data in testimonalSecond">
       <img :src="data.photo" :alt="data.username" />
       <p>{{ data.message }} - @{{ data.username }}</p>
     </div>
@@ -11,8 +16,22 @@ import testimonals from "@/data/testimonals";
 export default {
   data() {
     return {
-      testimonal: testimonals,
+      testimonalFirst: testimonals,
+      testimonalSecond: testimonals,
     };
+  },
+  methods: {
+    sliceTestimonialFirst: function () {
+      this.testimonalFirst = this.testimonalFirst.slice(0, 6);
+    },
+    sliceTestimonialSecond: function () {
+      this.testimonalSecond = this.testimonalSecond.slice(6, 12);
+    },
+  },
+  mounted() {
+    this.sliceTestimonialFirst();
+    this.sliceTestimonialSecond();
+    console.log(this.testimonalFirst);
   },
 };
 </script>
@@ -34,14 +53,26 @@ export default {
     border-radius: 45px;
     background-color: #fff;
     padding: 30px;
-    animation: slider 10000ms infinite ease;
-    @keyframes slider {
-      100% {
-        transform: translateX(-200%);
-      }
-    }
     img {
       width: 52px;
+    }
+  }
+  .testimonials.first {
+    position: relative;
+    right: 150px;
+    animation: first 9000ms infinite;
+    @keyframes first {
+      100% {
+        transform: translateX(-80%);
+      }
+    }
+  }
+  .testimonials.second {
+    animation: first 7000ms infinite;
+    @keyframes first {
+      100% {
+        transform: translateX(-80%);
+      }
     }
   }
 }

bf3c2128b1f6609798597f0b5bc128106245f476
Usmonov Azizbek <usmonovazizbek045@gmail.com>
2023-04-20 16:07:35 +0500

    Our team

--- src/components/team/ourTeam/OurTeam.vue
+++ src/components/team/ourTeam/OurTeam.vue
@@ -8,15 +8,33 @@
                 Meet With Professional Team
             </TitleInfo>
         </div>
+        <div>
+            
+        </div>
     </div>
 </template>
 
 <script>
+import team from "@/data/team"
 export default {
-    name: "TeamFirstIndex"
+    name: "TeamFirstIndex",
+    data() {
+        return {
+            team: team
+        }
+    },
+    mounted() {
+        console.log(this.team);
+    },
 }
 </script>
 
 <style lang="scss" scoped>
-    
+.title {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    align-items: center;
+}
 </style>
